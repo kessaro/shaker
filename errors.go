dc2ee6eaafe2b1ad7ef95b0d371971da635b531f
Arnaud DE MATTEIS <hello@arnauddematteis.fr>
2026-02-28 18:44:58 +0100

    feat: Register custom error states and map them to a custom http error code

--- errors.go
+++ errors.go
@@ -2,28 +2,9 @@ package shaker
 
 import (
 	"errors"
-	"fmt"
 )
 
-func ErrNotFound() errNotFound {
-	return errNotFound{}
-}
-
-func ErrNotFoundf(resourceName string) errNotFound {
-	return errNotFound{
-		ResourceName: resourceName,
-	}
-}
-
-type errNotFound struct {
-	ResourceName string
-}
-
-func (e errNotFound) Error() string {
-	if e.ResourceName == "" {
-		e.ResourceName = "resource"
-	}
-	return fmt.Sprintf("%s not found", e.ResourceName)
-}
-
-var ErrInvalidHandlerSignature = errors.New("invalid handler signature")
+var (
+	ErrRessourceNotFound       = errors.New("ressource not found")
+	ErrInvalidHandlerSignature = errors.New("invalid handler signature")
+)
